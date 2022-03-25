8e7a06b938bf6d0cf3bc5fd5e8209caa5890dad8
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2022-03-24 23:18:58 -0400

    Add inheritance class example

---
+++ PRACTICES/src/classes/animal.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def get_name(self):
+        return self.name
+
+    def get_color(self):
+        return self.color
+
+    def desplazar(self):
+        print("caminando....")
+
