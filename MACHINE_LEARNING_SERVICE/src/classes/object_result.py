1222ad89bdecc4eeaa725168d9d836d4ff430330
Alvaro Quena <alvaro.quena1@gmail.com>
2022-03-28 14:35:14 -0400

    Model Vgg16 last update

--- MACHINE_LEARNING_SERVICE/src/classes/object_result.py
+++ MACHINE_LEARNING_SERVICE/src/classes/object_result.py
@@ -13,32 +13,42 @@
 
 
 class ObjectResult:
-    def __init__(self, name="", percentage=0.0, path_file="//"):
+    """Class that represent the return of Model"""
+    def __init__(self, name="", percentage=0, path_file="//"):
+        """Constructor of ObjectResult"""
         self._name = name
         self._percentage = percentage
         self._path_file = path_file
         self._id_object = ''
 
+    # Return the name of the result
     def get_name(self):
         return self._name
 
+    # Set tne name of the result
     def set_name(self, name):
         self._name = name
 
+    # Return the percentage of the result
     def get_percentage(self):
         return self._percentage
 
+    # Set the percentage of the result
     def set_percentage(self, percentage):
         self._percentage = percentage
 
+    # Return the path of the file of the result
     def get_path_file(self):
         return self._path_file
 
+    # Set the path of the file of the result
     def set_path_file(self, path_file):
         self._path_file = path_file
 
+    # Return the id of the Result
     def get_id_object(self):
         return self._id_object
 
+    # Set the id of the result
     def set_id_object(self, id_object):
         self._id_object = id_object
