65c8f34ce8af22de586a671a082da99b546c7f9c
daylerVMT <98544442+daylerVMT@users.noreply.github.com>
2022-03-27 23:29:45 -0400

    Update file_downloader.py
    add .py

--- CONVERT_SERVICES/src/converter/controller/rest_api_dayler/resources/file_downloader.py
+++ CONVERT_SERVICES/src/converter/controller/rest_api_dayler/resources/file_downloader.py
@@ -19,7 +19,6 @@ class FileDownloader(Resource):
     def get(self, file_id):
         name = f"{file_id}.jpg"
         filepath = f"files/{name}"
-
         file = send_file(
             path_or_file=filepath,
             mimetype="application/octet-stream",
