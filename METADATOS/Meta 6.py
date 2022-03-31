2e9e1186e245f22fb53c571957393e5d6942c879
RIGO LAZO <ivanrlazom24@gmail.com>
2022-03-31 08:57:32 -0400

    add metadatos de imagenes solo extencion JSON

--- METADATOS/Meta 6.py
+++ METADATOS/Meta 6.py
@@ -15,11 +15,13 @@ import subprocess
 import os
 ##CREAMOS NUESTRA CLASE
 class Metadata:
+
 ### DEFINIMOS LAS ENTRADAS DE LA CLASE
     def __init__(self,path,ext,direct):
         self.path=path
         self.ext=ext
         self.direct=direct
+
 ### definimosla funcion
     def select_file(self):
         direc = os.listdir(self.path)## sacamos el nombre de cada archivo de la carpeta
@@ -27,15 +29,20 @@ class Metadata:
         for file in direc:
             if os.path.isfile(os.path.join(self.path,file)) and file.endswith(
                     self.ext):##limitamos el tipo de archivo que entrara con la extencion ".jpg"
+
                 name = str(file)
                 name2 = name[:-4]## eliminamos la extencion del archivo en el caso del ejemplo el
                 # ".jpg"
+
                 ##procesamos cada archivo con el Exiftool.exe y sacamos sus dependencias en
                 # formato JSON creando un archivo.JSON para cada imagen
                 process = subprocess.run([exe,str(self.path) + "/" + str(file), '-json', '-W+!',
                                             str(self.direct) + name2 + ".json"])
+
                 ## imprimimos el estado de cada conversion
                 print(process.stdout)
+
+
 ######### LLAMAMOS A NUESTRA CLASE CON LOS VALORES DE LA DIRECCION DE ENTRADA, LA EXTENCION Y LA
 # SALIDA
 prueba = Metadata("C:/IVAN/CODING/Metadatos/imagenes",'.jpg',"C:/IVAN/CODING/Metadatos/metaout/")
