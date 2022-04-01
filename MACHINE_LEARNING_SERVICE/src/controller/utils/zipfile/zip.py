57e07f4fa8b2fd3ccb7e58417aef15761b89a603
daylerVMT <98544442+daylerVMT@users.noreply.github.com>
2022-04-01 17:11:07 -0400

    Update zip.py
    it is already corrected

--- MACHINE_LEARNING_SERVICE/src/controller/utils/zipfile/zip.py
+++ MACHINE_LEARNING_SERVICE/src/controller/utils/zipfile/zip.py
@@ -1,15 +1,28 @@
+#
+# @main.py Copyright (c) 2022 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# Edificio Union № 1376 Av. General Inofuentes esquina Calle 20, La Paz, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 import zipfile
 
-# creacion archivo comprimido
+# creation of compressed files
 with zipfile.ZipFile("comprimidos.zip", "w") as fzip:
     fzip.write("calzip.txt")
     print("Archivos comprimidos")
-#listar contenido del archivo zip
+#list zip file content
 fzip = zipfile.ZipFile("comprimidos.zip")
 print(fzip.printdir())
 listaArchivos = fzip.namelist()
 print(listaArchivos)
-#obtenemos mas informacion
+#We get more information
 info = fzip.infolist()
 for archivo in info:
         print(archivo.filename.archivo.date_time,archivo.compress_size)
