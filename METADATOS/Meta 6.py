89df4999285326cbe3c0c582e30eb5736c0fa871
RIGO LAZO <ivanrlazom24@gmail.com>
2022-04-04 16:53:50 -0400

    add metadatos de archivos txt,json y xmp clase 1 variable

--- METADATOS/Meta 6.py
+++ METADATOS/Meta 6.py
@@ -14,38 +14,47 @@
 import subprocess
 import os
 
-
 ##CREAMOS NUESTRA CLASE
 class Metadata:
 
 ### DEFINIMOS LAS ENTRADAS DE LA CLASE
-    def __init__(self,path,ext,direct):
-        self.path=path
-        self.ext=ext
-        self.direct=direct
+    def __init__(self,meti):
+        self.path = "C:/IVAN/CODING/Metadatos/filein"
+        self.direct = "C:/IVAN/CODING/Metadatos/metaout/"
+        self.meti = meti
 
 ### definimosla funcion
     def select_file(self):
         direc = os.listdir(self.path)## sacamos el nombre de cada archivo de la carpeta
         exe = "Exiftool"##inicializamos el ejecutable como constante
         for file in direc:
-            if os.path.isfile(os.path.join(self.path,file)) and file.endswith(
-                    self.ext):##limitamos el tipo de archivo que entrara con la extencion ".jpg"
-
+            if os.path.isfile(os.path.join(self.path,file)):
                 name = str(file)
-                namef = name[:-4]## eliminamos la extencion del archivo en el caso del ejemplo el
+                namef=name.split('.')
+#                namef = name[:-5]## eliminamos la extencion del archivo en el caso del ejemplo el
                 # ".jpg"
-
+                if self.meti == 'json':
                 ##procesamos cada archivo con el Exiftool.exe y sacamos sus dependencias en
                 # formato JSON creando un archivo.JSON para cada imagen
-                process = subprocess.run([exe,str(self.path) + "/" + str(file), '-json', '-W+!',
-                                            str(self.direct) + namef + ".json"])
-
+                    process = subprocess.run([exe,str(self.path) + "/" + str(file), '-' + str(
+                        self.meti), '-W+!', str(self.direct) + namef[0] + '.' + str(self.meti)])
                 ## imprimimos el estado de cada conversion
-                print(process.stdout)
+                    print(process.stdout)
+                ###--******************************************************************************
+                elif self.meti == 'txt':
+                    process = subprocess.run([exe, str(self.path) + "/" + str(file), '-W+!',
+                                              str(self.direct) + namef[0] + '.' + str(self.meti)])
+                    ## imprimimos el estado de cada conversion
+                    print(process.stdout)
+                ##-********************************************************************************
+                else : ## si es XMP
+                    process = subprocess.run([exe, str(self.path) + "/" + str(file), '-o',
+                                               str(self.direct) + namef[0] + '.' + str(self.meti)])
+                    ## imprimimos el estado de cada conversion
+                    print(process.stdout)
 
 
 ######### LLAMAMOS A NUESTRA CLASE CON LOS VALORES DE LA DIRECCION DE ENTRADA, LA EXTENCION Y LA
 # SALIDA
-prueba = Metadata("C:/IVAN/CODING/Metadatos/imagenes",'.jpg',"C:/IVAN/CODING/Metadatos/metaout/")
+prueba = Metadata('xmp')
 prueba.select_file()
