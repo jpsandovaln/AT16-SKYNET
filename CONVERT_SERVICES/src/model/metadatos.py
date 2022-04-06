c24ee74352fd2b735f804387a34f7183727c13aa
RIGO LAZO <ivanrlazom24@gmail.com>
2022-04-05 22:14:53 -0400

    metadato develop folders in out

--- CONVERT_SERVICES/src/model/metadatos.py
+++ CONVERT_SERVICES/src/model/metadatos.py
@@ -11,42 +11,46 @@
 # with Jalasoft
 
 import subprocess
-import os
 
-##CREAMOS NUESTRA CLASE
+
+# CREAMOS NUESTRA CLASE
 class Metadata:
 
-### define the input of class
-    def __init__(self,meti):
-        self.path = "..//filein"
-        self.direct = "..//metadata_output"
-        self.meti = meti
+# define the input of class
+    def __init__(self, meta, dir):
+        self.path = "..//..//saved_files/metadata_upload" ## carpeta de entrada
+        self.file = dir
+        self.direct = "..//..//saved_files/metadata_download"## carpeta de salida
+        self.meti = meta
 
-### define function
-    def select_file(self):
-        direc = os.listdir(self.path)## charge the name of all files in folder
+### define function for extract metadatos
+    def Select_File(self):
+#       direc = os.listdir(self.path)## charge the name of all files in folder
         exe = "..//..//third_party/win/Exiftool"## rename executable
-        for file in direc:
-            if os.path.isfile(os.path.join(self.path,file)):
-                name = str(file)
-                namef=name.split('.')
+#        for file in direc:
+#            if os.path.isfile(os.path.join(self.path,file)):
+        name = str(self.file)
+        namef = name.split('.')
 
-                ##Process all files with exiftools.exe and extract Metadatos
+        ##Process all files with exiftools.exe and extract Metadatos
 
-                if self.meti == 'json':
-                    process = subprocess.run([exe,str(self.path) + "/" + str(file), '-' +
-                            str(self.meti), '-W+!', str(self.direct) + "/" + namef[0] + '.' +
-                                              str(self.meti)])
-                elif self.meti == 'txt':
-                    process = subprocess.run([exe, str(self.path) + "/" + str(file), '-W+!',
-                            str(self.direct) + "/" + namef[0] + '.' + str(self.meti)])
-                else:
-                    process = subprocess.run([exe, str(self.path) + "/" + str(file), '-O',
-                                    str(self.direct) + "/" + namef[0] + '.' + str(self.meti)])
-                print(process.stdout)
+        if self.meti == 'json':
+            process = subprocess.run([exe, str(self.path) + "/" + str(self.file), '-' +
+                                      str(self.meti), '-W+!', str(self.direct) + "/" + namef[0] +
+                                      '.' + str(self.meti)])
+        elif self.meti == 'txt':
+            process = subprocess.run([exe, str(self.path) + "/" + str(self.file), '-W+!',
+                                      str(self.direct) + "/" + namef[0] + '.' + str(self.meti)])
+        else:
+            process = subprocess.run([exe, str(self.path) + "/" + str(self.file), '-O',
+                                      str(self.direct) + "/" + namef[0] + '.' + str(self.meti)])
+        print(process.stdout)
 
+###( exe,entrada path,-jason,-W+!,salidapath/name.json)
+##(exe,entrada path,-W+!,salidapath/name.txt)
+###(exe, entradapath,-o,salidapath/name.xmp)
 
 ##### Call the object and run the function with 1 parameter, it is the extencion of output
 
-prueba = Metadata('txt')
-prueba.select_file()
+prueba = Metadata('xmp',"BugAdvocacy2008.pdf")
+prueba.Select_File()
