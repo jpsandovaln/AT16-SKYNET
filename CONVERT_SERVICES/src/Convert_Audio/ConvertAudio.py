8ac69f436c29d00525790e4000fba04d357f97da
Alex Castro <alexander72577@gmail.com>
2022-04-05 22:39:21 -0400

    Added the audio converter

---
+++ CONVERT_SERVICES/src/Convert_Audio/ConvertAudio.py
@@ -0,0 +1,54 @@
+#
+# @ConvertAudio.py Copyright (c)
+# 2643 Av  Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# 1376 Av General Inofuentes esquina calle 20, La Paz, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall # not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+from params import Params
+
+
+
+class ConvertAudio(Params):
+    def __init__(self, str_input, path_out, path_in):
+        super().__init__(str_input, path_out, path_in)
+        self.dictionary = Params.dic_str_input(self)
+        self.cmd_input_copy = self.compare_dic()
+        self.ffmpeg_cmd = self.cmd_ff()
+
+
+    def compare_dic(self):
+        dic_param = {'acodex': 'c:a {}',
+                     'bitrate': 'ab {}k ',
+                     'sambple-rate': 'ar {} ',
+                     'audio-channel': 'ac {} '}
+        cmd_input = "-"
+        for key, val in self.dictionary.items():
+            if key in dic_param:
+                cmd_input += dic_param[key].format(val) + '-'
+        cmd_input_copy = cmd_input[:-1]
+        return cmd_input_copy
+
+
+    def cmd_ff(self):
+        form_conv = ""
+        for key, val in self.dictionary.items():
+            if key == 'format':
+                form_conv = val
+        try:
+            ffmpeg_command = "ffmpeg -i {}  {} {}\Converted_Audio.{}".\
+                format(self.path_in, self.cmd_input_copy, self.path_out,
+                       form_conv)
+
+            import subprocess
+            subprocess.call(ffmpeg_command)
+            return True
+        except:
+            return False
+
---
+++ CONVERT_SERVICES/src/Convert_Audio/params.py
@@ -0,0 +1,32 @@
+#
+# @params.py Copyright (c)
+# 2643 Av  Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# 1376 Av General Inofuentes esquina calle 20, La Paz, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall # not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+# This class inherits the inputs of the converter.
+from converter import Converter
+
+
+class Params(Converter):
+    def __init__(self, str_input, path_out, path_in):
+        super().__init__(str_input, path_out, path_in)
+        self.spl_str_input = str_input.split(',')
+        self.dictionary = self.dic_str_input()
+
+# This method creates a dictionary of the input parameters.
+    def dic_str_input(self):
+        dictionary = {}
+        for k_v in self.spl_str_input:
+            if '=' in k_v:
+                k_v = k_v.split('=')
+                dictionary[(k_v[0]).strip().lower()] = (k_v[1])
+            else:
+                dictionary[(k_v[:]).strip().lower()] = ''
+        return dictionary
