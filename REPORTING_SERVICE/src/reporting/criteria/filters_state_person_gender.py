93fae2c071a17e9a3f0e0982d6fcce6770977cd7
Amilcar-B <Amilcar.Barriento@fundacion-jala.org>
2022-04-07 19:31:11 -0300

    Fix

--- REPORTING_SERVICE/src/reporting/criteria/filters_state_person_gender.py
+++ REPORTING_SERVICE/src/reporting/criteria/filters_state_person_gender.py
@@ -5,7 +5,7 @@
 # All rights reserved.
 #
 # This software is the confidential and proprietary information of
-# Jalasoft, ("Condidential Information"). You shall # not
+# Jalasoft, ("Confidential Information"). You shall # not
 # disclose such Confidential Information and shall use it only in
 # accordance with the terms of the license agreement you entered into
 # with Jalasoft.
