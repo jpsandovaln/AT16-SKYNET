16b9b889e60f394e6ce6e4e32c189db1388813c8
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2022-05-26 19:04:25 -0400

    Added code to integrate with compiler services using harcoding path

--- COMPILER_DESKTOP/src/com/jalasoft/compiler/controller/compiler_controller.py
+++ COMPILER_DESKTOP/src/com/jalasoft/compiler/controller/compiler_controller.py
@@ -1,3 +1,4 @@
+import requests
 from src.com.jalasoft.compiler.view.compiler_view import CompilerView
 
 
@@ -5,3 +6,12 @@ class CompilerController:
     def __init__(self):
         self.view = CompilerView()
         self.view.init_ui()
+
+        url = "http://127.0.0.1:5000"
+        file_path = "D:/EjemploJava8.java"
+        files = {'file': open(file_path, 'rb')}
+        payload = {'version': '', 'lang': 'java'}
+        response = requests.post(url, files=files, data=payload, verify=False)
+        resp = response.json()
+        message = resp['message']
+        self.view.get_main_widget().set_result(message)
