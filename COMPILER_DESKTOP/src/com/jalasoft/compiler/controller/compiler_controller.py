c224024aa52efa41c4918a0929e96d575f28ba78
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2022-05-27 22:26:52 -0400

    Get values

--- COMPILER_DESKTOP/src/com/jalasoft/compiler/controller/compiler_controller.py
+++ COMPILER_DESKTOP/src/com/jalasoft/compiler/controller/compiler_controller.py
@@ -6,11 +6,16 @@ class CompilerController:
     def __init__(self):
         self.view = CompilerView()
         self.view.init_ui()
+        self.view.get_main_widget().get_compiler_button().clicked.connect(self.show_data)
 
+    def show_data(self):
         url = "http://127.0.0.1:5000"
-        file_path = "D:/EjemploJava8.java"
+        file_path = self.view.get_main_widget().get_file_path()
+        lang = self.view.get_main_widget().get_languages()
+        version = self.view.get_main_widget().get_version()
+
         files = {'file': open(file_path, 'rb')}
-        payload = {'version': '', 'lang': 'java'}
+        payload = {'version': version, 'lang': lang}
         response = requests.post(url, files=files, data=payload, verify=False)
         resp = response.json()
         message = resp['message']
--- COMPILER_DESKTOP/src/com/jalasoft/compiler/view/buttons/primary_button.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QPushButton
-
-
-class PrimaryButton(QPushButton):
-    def __init__(self, label):
-        super().__init__()
-        self.setText(label)
-        self.setStyle("background: red")
